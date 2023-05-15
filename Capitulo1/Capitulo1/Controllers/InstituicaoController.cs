133997521bd124fce88a936fc2bd6a39b4549a32
Arthur Zimmermann de Oliveira <arthur.zimmermann11@gmail.com>
2023-05-15 18:06:49 -0300

    capitulo 1 ao 1.4

---
+++ Capitulo1/Capitulo1/Controllers/InstituicaoController.cs
@@ -0,0 +1,46 @@
+﻿using Capitulo1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Capitulo1.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao()
+            {
+                InstituicaoID = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao()
+            {
+                InstituicaoID = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },new Instituicao()
+            {
+                InstituicaoID = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "SãoPaulo"
+            },new Instituicao()
+            {
+                InstituicaoID = 4,
+                Nome = "UniSulGrandense",
+                Endereco = "Rio Grande do Sul"
+            },new Instituicao()
+            {
+                InstituicaoID = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(instituicoes);
+        }
+    }
+}
